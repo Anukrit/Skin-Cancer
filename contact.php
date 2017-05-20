b2118023101ab40d4f5a5e097af0b29be7241095
Udaymeduri <bhargavi.meduri@my.jcu.edu.au>
2017-05-20 22:15:58 +1000

    Create contact.php
    Creating the contact information of simone (Admin)

---
+++ contact.php
@@ -0,0 +1,34 @@
+
+<html>
+    <head>
+        <title>
+            Home
+        </title>
+    </head>
+<body>
+<?php 
+include_once('menu.php');
+?>
+<div align="center" style="margin:auto" class="content">
+	<h1>Contact us</h1>
+
+	<div style="display:inline-block; text-align: left;">
+		<p><b>e-mail:</b> jcuskincancerresearchgroup@gmail.com</p>
+		<p><b>phone:</b> 44331749</p>
+		<p><h2>Project Director:</h2>Dr Simone Harrison <br>
+		JCU Skin Cancer Research Unit <br>
+		College of Public Health, Medical and Veterinary Sciences
+		</p>
+		<p><b>e-mail:</b> Simone.harrison@jcu.edu.au</p>
+		<p><b>Mobile:</b> 0423 489 083</p>
+	</div>
+</div>
+<!--This code is used for changing the active item on the menu-->
+<script>
+$(document).ready(function(){
+$('#contact').addClass("active");
+});
+</script>
+<!-- The above code is used to change the active item on the menu. To be explicitly pasted on each page that is created-->
+</body>
+</html>
