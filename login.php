6ae0f81e1a9ac5333056bbd32fa6f241a294aa74
Anukrit <anukrit.gaur@my.jcu.edu.au>
2017-05-27 14:55:18 +1000

    Update login.php
    Some styling and bug fixes

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <html>
     <head>
         <title>
@@ -8,8 +9,10 @@
 <?php 
 include_once('menu.php');
 ?>
-<div align="center" style="margin:auto" class="content">
-<form action="login_process.php" method="post">
+
+
+<div align="" style="margin:auto" class="content">
+<form action="login_process.php" method="post" class="col-md-6 col-md-push-3 jumbotron">
 <?php if(isset($_SESSION['error'])){?>
 <h2>
 <?php 
@@ -19,9 +22,15 @@ unset($_SESSION['error']);
 </h2>
 <?php
 }?>
-				<p>User Name:<input type="text" name="username"></p>
-				<p>Password:<input type="password" name="password"></p>
-				<p><input type="submit" value="Login">
+	<div class="col-md-5 text-right  ">UserName: </div> 
+	<div class="col-md-7 margin-bottom-10"><input type="text" name="username"></div>
+
+	<div class="col-md-5 text-right  ">Password: </div> 
+	<div class="col-md-7 margin-bottom-10"><input type="password" name="password"></div>
+
+	<div class="col-md-5 text-right  "></div> 
+	<div class="col-md-7"><input type="submit" value="Login"></div>
+
 </form>
 </div>
 <script>
