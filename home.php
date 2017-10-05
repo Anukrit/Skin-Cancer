8a08d1a995ed4a63a6486d3261dc8543ed0064d4
Anukrit <anukrit.gaur@my.jcu.edu.au>
2017-10-06 00:33:03 +1000

    Final

--- home.php
+++ home.php
@@ -1,4 +1,7 @@
-
+<?php
+session_start();
+include_once('config.php');
+?>
 <html>
     <head>
         <title>
@@ -6,9 +9,15 @@
         </title>
     </head>
 <body>
-<?php 
+<?php
 include_once('menu.php');
 ?>
+<style>
+.carousel-caption{
+  position: absolute;
+  left: 0;
+}
+</style>
 <div align="center" style="margin:auto" class="content">
 
 <!-- Carousel
@@ -22,26 +31,30 @@ include_once('menu.php');
       </ol>
       <div class="carousel-inner" role="listbox">
         <div class="item active">
-          <img class="first-slide" src="images/blue-rules.jpg" alt="First slide">
+          <img class="first-slide" src="images/slider/Cross country June 05 UNBLURRED    WITH PERMISSION TO USE_censored_FINAL 28SEPT17.jpg" alt="First slide">
           <div class="container">
             <div class="carousel-caption">
               <h1 class="hidden">Example headline.</h1>
-              <p class="hidden">Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+              <p class="" style="padding:10px; background-color: red; color:white; float:left; opacity: .8;max-width: 708px;">Become a volunteer researcher &amp; help us keep Queensland kids sun-safe</p>
               <p class="hidden"><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
             </div>
           </div>
         </div>
         <div class="item">
-          <img class="second-slide" src="images/blue-card-information-banner.jpg" alt="Second slide">
+          <img class="second-slide" src="images/slider/IMG_3583 high school girls excursion cropped_censored_blurred 27sept17.jpg" alt="Second slide">
           <div class="container">
             <div class="carousel-caption">
+              <p class="" style="padding:10px; background-color: yellow; color:black; float:left;opacity: .9;max-width: 708px;">Devoting 5 mins/month to record hat-wearing practices at your local school can make a real difference.</p>
+
             </div>
           </div>
         </div>
         <div class="item">
-          <img class="third-slide" src="images/Working-with-Children-Check-Blue-Card-QLD.jpg" alt="Third slide">
+          <img class="third-slide" src="images/slider/Swimming pool censored.jpg" alt="Third slide">
           <div class="container">
             <div class="carousel-caption">
+              <p class="" style="padding:10px; background-color: purple; color:white; float:left;opacity: .8;max-width: 708px;">We assist you to complete the working with children check (blue card) &amp; provide all the research training you need online.</p>
+
             </div>
           </div>
         </div>
@@ -56,34 +69,50 @@ include_once('menu.php');
       </a>
     </div><!-- /.carousel -->
 
-
-<div class=" col-md-6 col-md-push-2 margin-auto" style="text-align:left;">
+<div class=" col-lg-7 col-lg-push-1 col-md-8 col-md-push-1 margin-auto" style="text-align:justify;">
 <h2>Become a volunteer researcher & help protect Queensland school children from a future burdened by skin cancer</h2>
-<ul>
+
+<div class="equal-fix-width" >
+	<span class="blue-box">Do you walk your dog or drive past your local school when the children are outdoors?</span>
+</div>
+<div class="equal-fix-width" >
+	<span class="blue-box">Do you drop-off and collect your own children/grandkids from school regularly?</span>
+</div>
+<div class="equal-fix-width" >
+	<span class="blue-box">Do you attend  athletics or swimming carnivals?</span>
+</div>
+
+<div class="equal-fix-width" >
+	<span class="blue-box">Perhaps you live or work near a school?</span>
+</div>
+
+<div class="col-sm-12">&nbsp;</div>
+
+<ul class="hidden">
 <li>Do you drop-off and collect your own children/grandkids from school regularly?</li>
 <li>Perhaps you live or work near a school?</li>
 <li>Do you walk your dog or drive past your local school when the children are outdoors?</li>
 <li>Do you attend school athletics &/or swimming carnivals?</li>
 </ul>
+<div class="clear-both"></div>
 
-<p><i>If so, devoting as little as <span class="color-red">3-5 minutes once a month</span> to counting hat-wearing practices at your local school is enough to help us improve sun-safety in Queensland schools.</i></p>
-<p><i>If you want to collect hat-wearing data more often (e.g. weekly) or at several different schools in the area, you are most welcome, but even small contributions are valued and appreciated.</i></p>
+<p style="font-size:17px;"><i>If so, devoting as little as <span class="color-red">3-5 minutes once a month</span> to counting hat-wearing practices at your local school is enough to help us improve sun-safety in Queensland schools.</i></p>
+<p style="font-size:17px;"><i>If you want to collect hat-wearing data more often (e.g. weekly) or at several different schools in the area, you are most welcome, but even small contributions are valued and appreciated.</i></p>
 
 <h3>What is involved?</h3>
-<ul>
+<ul style="font-size:17px;">
 <li>Provide your contact details by visiting our volunteer sign-up page <a href="register.php">here</a></li>
-<li>Obtain a current Blue Card for Working with Children, if you don’t already have one. It’s FREE! 
-<a href="">apply or renew your card here </a> Past/present teachers may be exempt </li>
-<li>Provide us with a copy of your Blue Card details for validation <<Current Blue Card holders press here>></li>
-<li>Download and read our brief training manual to learn how to collect reliable research data on the hat-wearing practices of Queensland schoolchildren and their adult role-models and about the importance of data confidentiality;</li>
-<li>Practice your new hat-counting research skills by completing a quick on-line quiz;</li>
+<li>Obtain a current Blue Card for Working with Children, if you don't already have one. It's FREE!
+<a href="downloads.php">apply or renew your card here</a> Past/present teachers may be exempt; </li>
+<li>Provide us with a copy of your Blue Card details for validation.</li>
+<li><a href="files/Observations%20of%20hat%20wearing%20manual%2018april17.pdf">Download and read our brief training manual</a> to learn how to collect reliable research data on the hat-wearing practices of Queensland schoolchildren and their adult role-models and about the importance of data confidentiality;</li>
 <li>Practice your new hat-counting research skills by completing a quick on-line quiz;</li>
 <li>After accomplishing these steps, you will be able to download our smartphone app so that you can use your phone to record, store and send hat research data to us. That way, each time you are at school when the children are outside, and have 3 minutes to spare, you will be all set to record data for the project. </li>
 </ul>
-	
+
 <h3>What happens to the data I collect? </h3>
-<p>The data you and other volunteer researchers collect will be managed in a central database. Data will be collated periodically by season and time of day (before school/ play breaks/ outdoor sport/ school dismissal observations) so that results can be reported back to the schools themselves. That way, school management can see how they are performing relative to other schools in the area.</p>
-	
+<p style="font-size:17px;">The data you and other volunteer researchers collect will be managed in a central database. Data will be collated periodically by season and time of day (before school/ play breaks/ outdoor sport/ school dismissal observations) so that results can be reported back to the schools themselves. That way, school management can see how they are performing relative to other schools in the area.</p>
+
 </div>
 <div class="col-md-2 col-md-push-2 ">
 <iframe id="uvindexwidget" src="http://www.sunsmart.com.au/uvalert/default.asp?version=australia" width="170" height="300" frameborder="0" scrolling="no" style="padding:0; margin: 0px; overflow:hidden;" allowtransparency="true"></iframe></textarea>
@@ -92,12 +121,14 @@ include_once('menu.php');
 
 
 <div class="col-md-12 project-sponsors">
-<h2>PROJECT SPONSORS </h2>
-	<img src="images/violets.png" /><img src="images/rotary.png" /><img src="images/rotary-humanity.png" /><img src="images/danger-sun.png" />
+  <h2>PROJECT SPONSORS </h2>
+  <img src="images/jcu-sponsor-logo.png" />
+  <img src="images/violets.png" />
+  <span class="" style="display:inline-table"><img src="images/rotary.png" /><br>THURINGOWA CENTRAL</span>
+  <img src="images/danger-sun.png" />
 </div>
 
 <div class="col-md-12">
-<p>Volunteer sign-up page <<GET STARTED>></p>
 </div>
 
 </div>
@@ -111,23 +142,3 @@ $('#home').addClass("active");
 <!-- The above code is used to change the active item on the menu. To be explicitly pasted on each page that is created-->
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- login_process.php
+++ login_process.php
@@ -1,61 +1,37 @@
 <?php
 //must appear BEFORE the <html> tag
 session_start();
-include_once('config.php');	
+include_once('config.php');
 
-if( isset($_POST["username"])&& isset($_POST["password"]) )
-{
-	
+if( isset($_POST["username"])&& isset($_POST["password"])){
 	$username = $_POST["username"];
-		
-	$password = $_POST["password"];
-	
-	if ($username && $password)
-	{
-	  // if the user has just tried to log in
-	
-	  //make the database connection
-	  $conn  = db_connect();	
-	  
-	  //make a query to check if user login successfully
-	  $sql = "select * from user_details where username='$username' and password='$password'";
-	  $result = $conn->query($sql);
-	  if ($result->fetch())
-	  {
-		// login successfully, keep the user's username
-		$_SESSION['valid_user'] = $username;
-		$sql1 = "select * from user_details where username='$username' and password='$password' and isadmin='Y'";
-		$result1 = $conn->query($sql);
-		if ($result->fetch())
-		{
+	$password = md5($_POST["password"]);
+
+	$sql = "select * from user_details where username='".$conn->real_escape_string($username)."' and password='$password'";
+//	die($sql);
+	$result = $conn->query($sql);
+	echo $conn->error;
+	if ($result->num_rows > 0) {
+	  $row = $result->fetch_assoc();
+		$_SESSION['userid'] = $row['userid'];
+		$_SESSION['email'] = $row['email'];
+		$_SESSION['username'] = $username;
+		if($row[isadmin]=='Y')
 			$_SESSION['isadmin'] = 1;
-		}
-	  }
 	}
 }
-if (isset($_SESSION['valid_user']))
-{
-  header("Location:home.php");  
+if (isset($_SESSION['userid'])){
+  header("Location:bluecard.php");
 }
-else
-{
-  if (isset($username))
-  {
-    // if user tried and failed to log in
-	$_SESSION['error']='Invalid username or password';
+else{
+  if (isset($username)){
+		$_SESSION['error']='Invalid username or password';
     header("Location:login.php");
   }
-  else 
-  {
+  else{
     // user has not tried to log in yet or has logged out
     echo "<b>You are not logged in</b><br>";
   }
-
 }
 
 ?>
-
-
-
-
-
