e26bd765ed3aa26b0b0db8e0c091058c6d090320
harpal singh <harpal_sbbsiet@yahoo.co.in>
2017-10-06 01:15:49 +1000

    Add files via upload

--- downloads.php
+++ downloads.php
@@ -1,24 +1,54 @@
-
+<?php
+session_start();
+include_once('config.php');
+?>
 <html>
     <head>
         <title>
-            Home
+            Downloads
         </title>
     </head>
 <body>
-<?php 
+<?php
 include_once('menu.php');
 ?>
 <div align="center" style="margin:auto" class="content">
-	<h1>Help</h1>
+	<h1>Downloads</h1>
 
 	<div style="display:inline-block; text-align: left;">
+    <div class="col-sm-12">
 	<ol>
-		<li><a href="files/Blue-card-application-partially-completed.pdf">Blue card application, partially completed</a>
+		<li><a href="files/Bluecard 2018.pdf" target="_blank">Blue card application, partially completed</a>
 			(Right click and save to save this pdf to your system)
 		</li>
+    <li><a href="files/DJAG039-Confirmation-of-identity.pdf" target="_blank">Confirmation of identity form</a>
+			(Right click and save to save this pdf to your system)
+		</li>
+    <li><a href="files/DJAG015-Request-to-consider-alternative-identification.pdf" target="_blank">A Request To Consider Alternative Identification form</a>
+			(Right click and save to save this pdf to your system)
+		</li>
+
+    <?php if($_SESSION[userid]){ ?>
+      <li>
+        <a href="http://skincancerresearch.jcubitgroup.com/download_app.php?userid=<?php echo $_SESSION[userid]; ?>">Download app</a>
+      </li>
+    <?php } ?>
+
 	</ol>
+</div>
 	</div>
+
+<div class="row">
+  <div class="col-sm-3 col-md-4">
+    <img class="img-responsive" src="images/Sid sunsafe toddler.jpg">
+  </div>
+  <div class="col-sm-3 col-md-4">
+    <img class="img-responsive" src="images/shari.png" >
+  </div>
+  <div class="col-sm-3 col-md-4">
+    <img  class="img-responsive" src="images/townsville beach.jpg">
+  </div>
+</div>
 </div>
 <!--This code is used for changing the active item on the menu-->
 <script>
