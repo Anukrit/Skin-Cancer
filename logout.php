3ae92ff56746788567695646fd9628f8d00fc216
PRIYA3107 <priya.mehta@my.jcu.edu.au>
2017-10-06 13:31:27 +1000

    Final

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
 	session_start();
 	unset($_SESSION["valid_user"]);
 	session_destroy();
-	echo 'Logged out successfully';
+	$_SESSION = null;
+	//echo 'Logged out successfully';
 	header("Location:login.php");
 ?>
